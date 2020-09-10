5f07b7f70198a70593a6b6f8fb0929808182d758
vscabell <vscabell@student.42sp.org.br>
2020-09-10 06:02:59 +0200

    c03

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strstr(char *str, char *to_find)
+char		*ft_strstr(char *str, char *to_find)
 {
 	int	i;
 	int	offset;
